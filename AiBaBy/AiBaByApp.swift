a476561c8bb66140e64e8907b720a50fb11981a4
Han-GR <2411596211@qq.com>
2025-08-24 17:32:37 +0800

    Initial Commit

---
+++ AiBaBy/AiBaByApp.swift
@@ -0,0 +1,32 @@
+//
+//  AiBaByApp.swift
+//  AiBaBy
+//
+//  Created by han han on 2025/8/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AiBaByApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ AiBaByTests/AiBaByTests.swift
@@ -0,0 +1,17 @@
+//
+//  AiBaByTests.swift
+//  AiBaByTests
+//
+//  Created by han han on 2025/8/24.
+//
+
+import Testing
+@testable import AiBaBy
+
+struct AiBaByTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
