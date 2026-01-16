26505fede286571903363a2c7e937eb2f000bd24
Han-GR <2411596211@qq.com>
2026-01-16 17:12:50 +0800

    更改关于每事每刻样式

--- Moments/Views/SettingsView.swift
+++ Moments/Views/SettingsView.swift
@@ -174,92 +174,114 @@ struct SettingsView: View {
 
 struct AboutView: View {
     @Environment(\.dismiss) private var dismiss
+    @State private var appVersion = ""
     
     var body: some View {
         NavigationStack {
-            VStack(spacing: 20) {
-                Image(systemName: "bubbles.and.sparkles")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 80, height: 80)
-                    .padding(20)
-                    .clipShape(Circle())
+            ZStack {
+                Color(.systemGroupedBackground)
+                    .ignoresSafeArea()
                 
-                Text(
-                    NSLocalizedString("app_name_cn", value: "每事每刻", comment: "")
-                )
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                
-                Text(
-                    NSLocalizedString("about_tagline", value: "记录物品的美好时光", comment: "")
-                )
-                    .font(.headline)
-                    .foregroundColor(.secondary)
-                
-                Spacer()
-                    .frame(height: 30)
-                
-                VStack(alignment: .leading, spacing: 15) {
-                    Text(
-                        NSLocalizedString(
-                            "about_description",
-                            value: "每事每刻是一款专为记录和管理您的物品而设计的应用。您可以在这里记录与您有关的任何事物,不要错过任何瞬间。",
-                            comment: ""
-                        )
-                    )
-                    
-                    Text(
-                        NSLocalizedString("about_features_title", value: "功能特点：", comment: "")
-                    )
-                        .fontWeight(.bold)
-                        .padding(.top, 10)
-                    
-                    FeatureRow(
-                        icon: "camera.fill",
-                        text: NSLocalizedString(
-                            "about_feature_capture_photos",
-                            value: "拍照并上传物品的照片",
-                            comment: ""
-                        )
-                    )
-                    FeatureRow(
-                        icon: "pencil",
-                        text: NSLocalizedString(
-                            "about_feature_edit_info",
-                            value: "编辑物品的基本信息",
-                            comment: ""
-                        )
-                    )
-                    FeatureRow(
-                        icon: "heart.text.square.fill",
-                        text: NSLocalizedString(
-                            "about_feature_record_moments",
-                            value: "记录物品的生活瞬间",
-                            comment: ""
-                        )
-                    )
-                    FeatureRow(
-                        icon: "house.fill",
-                        text: NSLocalizedString(
-                            "about_feature_view_all_items",
-                            value: "在主页查看所有物品",
-                            comment: ""
-                        )
-                    )
+                ScrollView {
+                    VStack(spacing: 24) {
+                        Image(systemName: "bubbles.and.sparkles")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 80, height: 80)
+                            .padding(20)
+                            .background(Color(.secondarySystemBackground))
+                            .clipShape(Circle())
+                        
+                        VStack(spacing: 8) {
+                            Text(
+                                NSLocalizedString("app_name_cn", value: "每事每刻", comment: "")
+                            )
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                            
+                            Text(
+                                NSLocalizedString("about_tagline", value: "记录物品的美好时光", comment: "")
+                            )
+                                .font(.headline)
+                                .foregroundColor(.secondary)
+                            
+                            if !appVersion.isEmpty {
+                                Text(
+                                    String(
+                                        format: NSLocalizedString(
+                                            "version_format",
+                                            value: "版本 %@",
+                                            comment: ""
+                                        ),
+                                        appVersion
+                                    )
+                                )
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                            }
+                        }
+                        
+                        VStack(alignment: .leading, spacing: 16) {
+                            Text(
+                                NSLocalizedString(
+                                    "about_description",
+                                    value: "每事每刻是一款专为记录和管理您的物品而设计的应用。您可以在这里记录与您有关的任何事物,不要错过任何瞬间。",
+                                    comment: ""
+                                )
+                            )
+                            
+                            Text(
+                                NSLocalizedString("about_features_title", value: "功能特点：", comment: "")
+                            )
+                                .fontWeight(.bold)
+                                .padding(.top, 4)
+                            
+                            FeatureRow(
+                                icon: "camera.fill",
+                                text: NSLocalizedString(
+                                    "about_feature_capture_photos",
+                                    value: "拍照并上传物品的照片",
+                                    comment: ""
+                                )
+                            )
+                            FeatureRow(
+                                icon: "pencil",
+                                text: NSLocalizedString(
+                                    "about_feature_edit_info",
+                                    value: "编辑物品的基本信息",
+                                    comment: ""
+                                )
+                            )
+                            FeatureRow(
+                                icon: "heart.text.square.fill",
+                                text: NSLocalizedString(
+                                    "about_feature_record_moments",
+                                    value: "记录物品的生活瞬间",
+                                    comment: ""
+                                )
+                            )
+                            FeatureRow(
+                                icon: "house.fill",
+                                text: NSLocalizedString(
+                                    "about_feature_view_all_items",
+                                    value: "在主页查看所有物品",
+                                    comment: ""
+                                )
+                            )
+                        }
+                        .padding()
+                        .background(Color(.secondarySystemBackground))
+                        .cornerRadius(16)
+                        .padding(.horizontal)
+                        
+                        Text("© 2025 Han-GR")
+                            .font(.caption)
+                            .foregroundColor(.secondary)
+                            .padding(.top, 8)
+                    }
+                    .padding()
                 }
-                .padding()
-                .background(Color(.secondarySystemBackground))
-                .cornerRadius(12)
-                .padding(.horizontal)
-                
-                Spacer()
-                
-                Text("© 2025 Han-GR")
-                    .font(.caption)
-                    .foregroundColor(.secondary)
             }
-            .padding()
             .navigationTitle(
                 NSLocalizedString("title_about_full", value: "关于每事每刻", comment: "")
             )
@@ -272,6 +294,13 @@ struct AboutView: View {
                         dismiss()
                     }
                 }
+            }
+        }
+        .onAppear {
+            if let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
+                appVersion = version
+            } else {
+                appVersion = "1.0"
             }
         }
     }
