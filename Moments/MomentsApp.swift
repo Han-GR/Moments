017190e00ff1f53ffaf4b39b083e6677a60876ac
Han-GR <2411596211@qq.com>
2026-01-23 17:57:40 +0800

    修复崩溃问题

--- Moments/MomentsApp.swift
+++ Moments/MomentsApp.swift
@@ -19,6 +19,14 @@ struct MomentsApp: App {
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
         do {
+            // Ensure the Application Support directory exists
+            let fileManager = FileManager.default
+            if let supportDir = fileManager.urls(for: .applicationSupportDirectory, in: .userDomainMask).first {
+                if !fileManager.fileExists(atPath: supportDir.path) {
+                    try fileManager.createDirectory(at: supportDir, withIntermediateDirectories: true, attributes: nil)
+                }
+            }
+            
             return try ModelContainer(for: schema, configurations: [modelConfiguration])
         } catch {
             fatalError("Could not create ModelContainer: \(error)")
